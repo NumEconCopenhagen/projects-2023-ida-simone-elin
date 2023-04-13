0951020393d8cd08c12bf4ffee03ef47ab10fbdc
wzr768 <wzr768@alumni.ku.dk>
2023-04-13 10:32:39 +0200

    nyny

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,3 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
+import plotly.graph_objects as go
+import ipywidgets as widgets
+
 
 #Cleaning the 4 datasets simultaneously
 def dataclean(df,value):
