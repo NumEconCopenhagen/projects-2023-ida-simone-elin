62b46b08e268eea81030d1a163e73eb464f81cd0
wzr768 <wzr768@alumni.ku.dk>
2023-04-13 10:06:17 +0200

    ny

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,23 +1,13 @@
-def keep_regs(df, regs):
-    """ Example function. Keep only the subset regs of regions in data.
 
-    Args:
-        df (pd.DataFrame): pandas dataframe 
+#Cleaning the 4 datasets simultaneously
+def dataclean(df,value):
+   """function that drops and renames columns"""
+   drop_these = ['INDICATOR','MEASURE','FREQUENCY','Flag Codes'] 
+   df.drop(drop_these, axis=1, inplace=True)
+   df.rename(columns={'LOCATION':'country', 'TIME':'year', 'Value':value}, inplace=True)   
+   return df
 
-    Returns:
-        df (pd.DataFrame): pandas dataframe
 
-    """ 
-    
-    for r in regs:
-        I = df.reg.str.contains(r)
-        df = df.loc[I == False] # keep everything else
-    
-    return df
 
-#import pandas as pd
 
-#data = ['alcohol','smokers','lifeexp65','socsupport']
-#for i in data:
- #   data[i] = pd.read_csv(f"{i}.csv")
-  #  print(i)
+
