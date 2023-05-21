f08f95a7006a5993114f602431a437a3d794e52b
wzr768 <wzr768@alumni.ku.dk>
2023-05-21 18:12:53 +0200

    indsat function tekst

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -8,7 +8,17 @@ import ipywidgets as widgets
 
 #Cleaning the 4 datasets simultaneously
 def dataclean(df,value):
-   """function that drops and renames columns"""
+   """function that drops and renames columns
+   
+   Args:
+      df :     Uncleaned pandas dataframe
+      value :  Different values specific to each dataset
+   
+   Returns:
+      df :     Cleaned pandas dataframe
+   
+   """
+
    drop_these = ['INDICATOR','MEASURE','FREQUENCY','Flag Codes'] 
    df.drop(drop_these, axis=1, inplace=True)
    df.rename(columns={'LOCATION':'country', 'TIME':'year', 'Value':value}, inplace=True)   
