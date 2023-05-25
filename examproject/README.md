25a6976082a09457e27f8154a0de8b32ad376b9d
hpd512 <124739512+hpd512@users.noreply.github.com>
2023-05-25 10:00:00 +0200

    first

---
+++ examproject/README.md
@@ -0,0 +1,7 @@
+# Exam project
+
+Our project is 
+
+The **results** of the project can be seen from running [modelproject.ipynb](modelproject.ipynb).
+
+**Dependencies:** Apart from a standard Anaconda Python 3 installation, the project requires no further packages.
---
+++ examproject/exam.py
@@ -0,0 +1,10 @@
+from scipy import optimize
+import numpy as np
+import sympy as sm
+from sympy.solvers import solve
+from sympy import Symbol
+import matplotlib.pyplot as plt
+from types import SimpleNamespace
+from tabulate import tabulate
+import ipywidgets as widgets
+from ipywidgets import interact, interactive, fixed, interact_manual
